33d3d0802fc18fa4aa6cceb8ee43c8b1b8cbb0b0
tjisa2 <jisatess@gmail.com>
2023-05-07 15:17:06 -0500

    progress!
    Buttons working enough- existing bugs are first row is double pressing and encoders getting pulled high by something in init.
    Made files for triangle and rectangular waves. Adjusted SD card read in sizes accordingly

--- Synthesizer/main.c
+++ Synthesizer/main.c
@@ -6,31 +6,148 @@
 #include "LCD.h"
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include "hardware/pwm.h"
+#include "hardware/irq.h"
+#include "hardware/sync.h"
+#include "hardware/adc.h"
 #include "imageN.h"
 // #include "font24.h"
-#include "menuButtons.h"
 #include "OLED.h"
 #include "sd_card.h"
 #include "ff.h"
 #include "MCP23017.h"
-//#include "pwm_audio_setup.h"
-// #include "pico_button_matrix.h"
 
+#define MainMenu 5
+#define SD 0
+#define SPECTRUM 1
+#define BEEPS 2
+#define BOOPS 3
+#define BOINKS 4
 #define ROWS 6
 #define COLS 5
 #define GPIO_INPUT false
 #define GPIO_OUTPUT true
-uint8_t g_buttonPress = 0; 
-uint row_pin;
-uint columns[5] = { 22, 23, 24, 25, 12 };
-uint rows[6] = { 26, 27, 28, 29, 11, 13 };
+#define SIZE_OF_BUFFER 16
+#define SW1 0
+#define SW2 5
+#define SW3 10
+#define SW4 15
+#define SW5 1
+#define SW6 6
+#define SW7 11
+#define SW8 2
+#define SW9 7
+#define SW10 12
+#define SW11 3
+#define SW12 8
+#define SW13 13
+#define UP 20
+#define DOWN 24
+#define LEFT 22
+#define RIGHT 23
+#define CENTER 21
+#define RE1 27
+#define RE2 29
+#define RE3 26
+#define RE4 28
+#define USER_INTERFACE 25
+#define AUDIO_PIN 2
+#define ADC_CHANNEL 2
+int g_buttonPress = 0; 
+uint columns[5] = { 25, 24, 23, 22, 12 };
+uint rows[6] = { 29, 28, 27, 26, 11, 13 };
+uint button_press=0;
+int rowState=0;
+int pinState=0;
+uint8_t buttonValues[30]={0};
+uint8_t prevButtonValues[30]={0};
+//ring buffer
+int flag=1;
+uint8_t ring_buffer[SIZE_OF_BUFFER]={0};
+int bufferLength = 0;
+int readIndex = 0;
+int writeIndex = 0;
+int changedKey=-1;
+int index1=0;
+int index2=0;
+int mod0;
+int row_index=0;
+int col_index=0;
+int row_pin=-1;
+int col_pin=-1;
+int row_pressed;
+int cur_sample = 0;
+uint16_t * audioTable;
+uint16_t AUDIO_SAMPLES = 0;
+uint16_t outputVal[10000]={0}; //check sizes for if this will output, check also the highest possible combo between the sound lengths
+
+
 MCP23017 gpio_expander;
 
 void printMenuOptions();
 void menuButtons_init(void);
-void buttonHandler(uint gpio, uint32_t events);
 void irq_en(bool en);
+//bool repeating_timer_callback(struct repeating_timer *t); 
+void *memset(void *str, int c, size_t n);
+void pwm_irh();
 
+// ISR for buttons. 
+bool repeating_timer_callback(struct repeating_timer *t) 
+{ 
+    //printf("line 664   ");
+    //check button states
+    for (col_index = 0; col_index < COLS; col_index++) //loop through rows
+    {
+       
+        gpio_put(columns[col_index], 1);
+        
+        //printf("\n    225\n\n    ");
+        for(row_index=0; row_index<ROWS; row_index++)//loop through rows
+        {
+            rowState=gpio_get(rows[row_index]);
+            //printf("\n    rowstate: %d\n\n    ",rowState);
+            // if (bufferLength == SIZE_OF_BUFFER) {
+			// 	printf("\n    Buffer is full!\n\n    ");
+			// 	continue;
+			// }
+            if(rowState ==1) //button pressed
+            {
+                buttonValues[row_index*5+col_index]=1;
+                //printf("\n    237\n\n    ");
+                if(prevButtonValues[row_index*5+col_index]==0)
+                {
+                    ring_buffer[writeIndex] =row_index*5+col_index; //store this value into ring buffer sequentially
+                    printf("\n    row state 1 %d\n\n    ", ring_buffer[writeIndex]);
+                   
+                     //bufferLength++;
+                }
+            }
+            if(rowState ==0) //button released
+            {
+                buttonValues[row_index*5+col_index]=0;
+                //printf("\n    249\n\n    ");
+                if(prevButtonValues[row_index*5+col_index]==1)
+                {
+                    ring_buffer[writeIndex]=row_index*5+col_index;
+                     printf("\n    row state 0 %d\n\n    ", ring_buffer[writeIndex]);
+                    
+                     //bufferLength++;
+                }
+            }
+            prevButtonValues[row_index*5+col_index]=buttonValues[row_index*5+col_index];
+           
+            writeIndex++;
+            // If at last index in buffer, set writeIndex back to 0
+            if (writeIndex == SIZE_OF_BUFFER) 
+            {
+                writeIndex = 0;
+            }
+        }
+        gpio_put(columns[col_index], 0);
+        //printf("\n    267\n\n    ");
+    }
+    //return false;
+}
 
 int main(void){
 
@@ -39,25 +156,56 @@ int main(void){
     FIL fil;
     //int ret;
     char buf2[10000];
-    uint16_t samples1[1000];
-    uint16_t samples2[1000];
-    uint16_t samples3[1000];
-    uint16_t samples4[1000];
-    uint16_t samples5[1000];
-    uint16_t samples6[1000];
-    uint16_t samples7[1000];
-    uint16_t samples8[1000];
-    uint16_t samples9[1000];
-    uint16_t samples10[1000];
-    uint16_t samples11[1000];
-    uint16_t samples12[1000];
-    uint16_t samples13[1000];
+    uint16_t samples1[84];
+    uint16_t samples2[80];
+    uint16_t samples3[76];
+    uint16_t samples4[73];
+    uint16_t samples5[70];
+    uint16_t samples6[68];
+    uint16_t samples7[65];
+    uint16_t samples8[63];
+    uint16_t samples9[60];
+    uint16_t samples10[59];
+    uint16_t samples11[57];
+    uint16_t samples12[55];
+    uint16_t samples13[54];
 
     char* sample;
     char filename[] = "sineWaveOctave.txt";
     spi_inst_t *spi = spi0;
 
     stdio_init_all();
+
+        //ISR for checking buttons
+    struct repeating_timer timer;
+    add_repeating_timer_ms(100, repeating_timer_callback, NULL, &timer);
+    memset(buttonValues,0,30);
+    memset(prevButtonValues,0,30);
+    memset(ring_buffer,0,16*sizeof(ring_buffer[0]));
+
+    //PWM Set up
+    gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
+
+    int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
+
+    // Setup PWM interrupt to fire when PWM cycle is complete
+    pwm_clear_irq(audio_pin_slice);
+    pwm_set_irq_enabled(audio_pin_slice, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_irh);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    // Setup PWM for audio output
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 5.5f);
+    pwm_config_set_wrap(&config, 254);
+    pwm_init(audio_pin_slice, &config, true);
+
+    pwm_set_gpio_level(AUDIO_PIN, 0);
+    
+    // end PWM setup
+
+    //init button matrix
+    menuButtons_init();
 
     LCD_init();
     Paint_DrawImage(gImage_imageN, 0,0,LCD_WIDTH,LCD_HEIGHT);
@@ -82,14 +230,14 @@ int main(void){
     // Initialize SD card
     if (!sd_init_driver()) {
         printf("ERROR: Could not initialize SD card\r\n");
-        while (true);
+        //while (true);
     }
 
     // Mount drive
     fr = f_mount(&fs, "0:", 1);
     if (fr != FR_OK) {
         printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
-        while (true);
+        //while (true);
     }
     
     spi_init(spi, 125000000);
@@ -133,8 +281,15 @@ int main(void){
         *   input, and nested switch statements to determine the 
         *   action based on the input. 
         */
-        switch (g_buttonPress)
+        g_buttonPress=ring_buffer[readIndex];
+        //printf("line 228 %d\n", g_buttonPress);
+        if(buttonValues[g_buttonPress])
         {
+            
+            if(g_buttonPress!=-1)
+            {
+            switch (g_buttonPress)
+            {
             case RIGHT:/* code */  //Right UI button
                 switch (menuLocation)
                 {
@@ -160,7 +315,7 @@ int main(void){
                     default:
                         break;
                 }
-                g_buttonPress = -1;
+                g_buttonPress=-1;
             break;
             case LEFT:/* code */    //left AKA back button
                 switch (menuLocation)
@@ -187,7 +342,7 @@ int main(void){
                     default:
                         break;
                 }
-            g_buttonPress = -1;
+            g_buttonPress=-1;
             break;
             case UP:/* code */ // UP 
                 switch (menuLocation)
@@ -196,6 +351,7 @@ int main(void){
                     if(squareY > 0 )
                     {
                         prevY = squareY;
+                        
                         squareY--;
                         irq_en(false);
                         Paint_DrawRectangle(39,prevY*20+51,200,prevY*20+51+19,MAGENTA,2,0);
@@ -207,9 +363,10 @@ int main(void){
                 default:
                     break;
                 }
-            g_buttonPress = -1;
+            g_buttonPress=-1;
             break;
             case DOWN:/* code */
+            printf("369\n");
                 switch (menuLocation)
                 {
                 case MainMenu:
@@ -217,6 +374,7 @@ int main(void){
                     {
                         prevY = squareY;
                         squareY++;
+                        printf("in down %d\n", squareY);
                         irq_en(false);
                         Paint_DrawRectangle(39,prevY*20+51,200,prevY*20+51+19,MAGENTA,2,0);
                         Paint_DrawRectangle(39,squareY*20+51,200,squareY*20+51+19,BLACK,2,0);
@@ -227,7 +385,7 @@ int main(void){
                 default:
                     break;
                 }
-            g_buttonPress = -1;
+            g_buttonPress=-1;
             break;
             case CENTER:/* code */
                 switch (menuLocation)
@@ -239,29 +397,78 @@ int main(void){
                 default:
                     break;
                 }
-            g_buttonPress = -1;
+            g_buttonPress=-1;
             break;
-            // case RE:/* code */
-            //     oled_render(buf, &frame_area);
-            //     oled_setCursor(0,2);
-            //     oled_WriteString16("***Nebraska?***\n");
-            //     g_buttonPress = -1;
-            // break;
-            // case RERIGHT:/* code */
-            //     oled_render(buf, &frame_area);
-            //     oled_setCursor(0,2);
-            //     oled_WriteString16("ADSR RIGHT\n");
-            //     g_buttonPress = -1;
-            // break;
-            // case RELEFT:/* code */
-            //     oled_render(buf, &frame_area);
-            //     oled_setCursor(0,2);
-            //     oled_WriteString16("ADSR LEFT\n");
-            //     g_buttonPress = -1;
-            // break;
-            default:
 
-            break;
+
+            case SW1:  //c4
+                audioTable=samples1;
+                AUDIO_SAMPLES=sizeof(samples1/sizeof(samples1[1]));
+                break;
+            case SW2: //c#
+                /* code */
+                break;
+            case SW3: //d
+                /* code */
+                break;
+            case SW4: //d#
+                /* code */
+                break;
+            case SW5: //e
+                /* code */
+                break;
+            case SW6: //f
+                /* code */
+                break;
+            case SW7: //f#
+                /* code */
+                break;
+            case SW8: //g
+                /* code */
+                break;
+            case SW9: //g#
+                /* code */
+                break;
+            case SW10: //a
+                /* code */
+                break; //a#
+            case SW11: //b
+                /* code */
+                break;
+            case SW12: //b#
+                /* code */
+                break;
+            case SW13: //c5
+                /* code */
+                break;
+            case RE1:
+                /* code */
+                break;
+            case RE2:
+                /* code */
+                break;
+            case RE3:
+                /* code */
+                break;
+            case RE4:
+                /* code */
+                break;
+            case USER_INTERFACE:
+                /* code */
+                break;
+            default:
+            //printf("in default, key is %d\n", g_buttonPress);
+                break;
+            }
+            g_buttonPress=-1;
+            }
+        }
+        bufferLength--;	 //	Decrease buffer size after reading
+        readIndex++;	 //	Increase readIndex position to prepare for next read
+
+        // If at last index in buffer, set readIndex back to 0
+        if (readIndex == SIZE_OF_BUFFER) {
+            readIndex = 0;
         }
         /*
         *   Menu action switch
@@ -501,7 +708,7 @@ void menuButtons_init(void)
         gpio_init(columns[i]);
 
         gpio_set_dir(columns[i], GPIO_OUTPUT);
-        gpio_put(columns[i], 1);
+        gpio_put(columns[i], 0);
     }
     
     for (int i = 0; i < ROWS; i++) 
@@ -515,82 +722,26 @@ void menuButtons_init(void)
     irq_en(true);
 }
 
-// ISR for buttons. 
-void buttonHandler(uint gpio, uint32_t events)
-{
-    for (int i = 0; i < ROWS; i++) 
-    {
-        if (rows[i] == gpio) 
-        {
-            row_pin = i;
-            break;
-        }
-    }
-    gpio_put(columns[0],1);
-    gpio_put(columns[1],0);
-    gpio_put(columns[2],0);
-    gpio_put(columns[3],0);
-    gpio_put(columns[4],0);
-    for(volatile uint32_t i = 0;i<1000;i++){}
-    if(gpio_get(gpio)==1)
-    {
-        g_buttonPress=row_pin*5;
-        
-    }
-    gpio_put(columns[0],0);
-    gpio_put(columns[1],1);
-    gpio_put(columns[2],0);
-    gpio_put(columns[3],0);
-    gpio_put(columns[4],0);
-    for(volatile uint32_t i = 0;i<1000;i++){}
-    if(gpio_get(gpio)==1)
-    {
-        g_buttonPress=row_pin*5+1;
-        
-    }
-    gpio_put(columns[0],0);
-    gpio_put(columns[1],0);
-    gpio_put(columns[2],1);
-    gpio_put(columns[3],0);
-    gpio_put(columns[4],0);
-    for(volatile uint32_t i = 0;i<1000;i++){}
-    if(gpio_get(gpio)==1)
-    {
-        g_buttonPress=row_pin*5+2;
-        
-    }
-    gpio_put(columns[0],0);
-    gpio_put(columns[1],0);
-    gpio_put(columns[2],0);
-    gpio_put(columns[3],1);
-    gpio_put(columns[4],0);
-    for(volatile uint32_t i = 0;i<1000;i++){}
-    if(gpio_get(gpio)==1)
-    {
-        g_buttonPress=row_pin*5+3;
 
+
+void pwm_irh() {
+    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));
+    pwm_set_gpio_level(AUDIO_PIN, audioTable[cur_sample >> 2]);
+
+
+    if (cur_sample < (AUDIO_SAMPLES * 4) - 1) {
+        ++cur_sample;
+       
+    } else {
+        cur_sample = 0;
     }
-    gpio_put(columns[0],0);
-    gpio_put(columns[1],0);
-    gpio_put(columns[2],0);
-    gpio_put(columns[3],0);
-    gpio_put(columns[4],1);
-    for(volatile uint32_t i = 0;i<1000;i++){}
-    if(gpio_get(gpio)==1)
-    {
-        g_buttonPress=row_pin*5+4;
-    }
-    gpio_put(columns[0],1);
-    gpio_put(columns[1],1);
-    gpio_put(columns[2],1);
-    gpio_put(columns[3],1);
-    gpio_put(columns[4],1);
-    for(volatile uint32_t i = 0;i<10000;i++){}
 }
+
 void irq_en(bool en)
 {   
-    for (int i = 0; i < 6; i++)
-    {
-        gpio_set_irq_enabled_with_callback(rows[i],GPIO_IRQ_EDGE_RISE,en,&buttonHandler);
-    }
+    // irq_set_enabled (PWM_IRQ_WRAP, en);
+    // irq_set_enabled (TIMER_IRQ_0, en);
+    // irq_set_enabled (TIMER_IRQ_1, en);
+    // irq_set_enabled (TIMER_IRQ_2, en);
+    // irq_set_enabled (TIMER_IRQ_3, en);
 }
